6cb7c6936f6a439d53b482d937a8c1c8b579d71c
Arjav <arjavbadjate1709@gmail.com>
2025-08-04 11:47:56 +0530

    updates

--- tasktracker/tasktracker/settings.py
+++ tasktracker/tasktracker/settings.py
@@ -1,99 +1,4 @@
-# from pathlib import Path
-# import os
 
-# BASE_DIR = Path(__file__).resolve().parent.parent
-
-# SECRET_KEY = 'your-secure-secret-key'
-# DEBUG = True
-
-# ALLOWED_HOSTS = ['*']  # Use specific domains in production
-
-# SHARED_APPS = (
-#     'django_tenants',
-#     'customers', 
-#     'django.contrib.contenttypes',
-#     'django.contrib.staticfiles',
-#     'tailwind',
-#     # Add additional shared apps here
-# )
-
-# TENANT_APPS = (
-#     'django.contrib.admin',
-#     'django.contrib.auth',
-#     'django.contrib.contenttypes',
-#     'django.contrib.sessions',
-#     'django.contrib.messages',
-#     'tracker',              # Make sure this exists!
-# )
-
-# INSTALLED_APPS = list(SHARED_APPS) + [a for a in TENANT_APPS if a not in SHARED_APPS]
-
-# TENANT_MODEL = "customers.Client"         # app.Model
-# TENANT_DOMAIN_MODEL = "customers.Domain"  # app.Model
-
-# MIDDLEWARE = [
-#     'django_tenants.middleware.main.TenantMainMiddleware',  # must be first
-#     'django.middleware.security.SecurityMiddleware',
-#     'django.contrib.sessions.middleware.SessionMiddleware',
-#     'django.middleware.common.CommonMiddleware',
-#     'django.middleware.csrf.CsrfViewMiddleware',
-#     'django.contrib.auth.middleware.AuthenticationMiddleware',
-#     'django.contrib.messages.middleware.MessageMiddleware',
-#     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-# ]
-
-# ROOT_URLCONF = 'tasktracker.urls'
-# # PUBLIC_SCHEMA_URLCONF = 'tasktracker.urls_public'  # Uncomment if using
-
-# WSGI_APPLICATION = 'tasktracker.wsgi.application'
-
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django_tenants.postgresql_backend',
-#         'NAME': 'tasktracker',
-#         'USER': 'postgres',
-#         'PASSWORD': 'postgres',
-#         'HOST': 'localhost',
-#         'PORT': '5432',
-#     }
-# }
-
-# DATABASE_ROUTERS = (
-#     'django_tenants.routers.TenantSyncRouter',
-# )
-# EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-# EMAIL_HOST = 'smtp.gmail.com'
-# EMAIL_PORT = 587
-# EMAIL_USE_TLS = True
-# EMAIL_HOST_USER = 'arjavbadjate1404@gmail.com'
-# EMAIL_HOST_PASSWORD = 'tvqy nozn ydvt yxrc'  # Not your Gmail password
-
-# TEMPLATES = [
-#     {
-#         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-#         'DIRS': [os.path.join(BASE_DIR, 'templates')],
-#         'APP_DIRS': True,
-#         'OPTIONS': {
-#             'context_processors': [
-#                 'django.template.context_processors.request',
-#                 'django.contrib.auth.context_processors.auth',
-#                 'django.contrib.messages.context_processors.messages',
-#             ],
-#         },
-#     },
-# ]
-# LOGIN_URL = '/login/'
-# LOGIN_REDIRECT_URL = '/'
-# LANGUAGE_CODE = 'en-us'
-# TIME_ZONE = 'UTC'
-# USE_I18N = True
-# USE_TZ = True
-
-# STATIC_URL = '/static/'
-# STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
-# STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-
-# DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
 from pathlib import Path
@@ -159,6 +64,16 @@ DATABASES = {
         ssl_require=True
     )
 }
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django_tenants.postgresql_backend',
+#         'NAME': 'tasktracker',
+#         'USER': 'postgres',
+#         'PASSWORD': 'postgres',
+#         'HOST': 'localhost',
+#         'PORT': '5432',
+#     }
+# }
 DATABASES['default']['ENGINE'] = 'django_tenants.postgresql_backend'
 
 DATABASE_ROUTERS = ('django_tenants.routers.TenantSyncRouter',)
